055e035db79deb5b10e430bfe4002ff38d1d6b89
leejihyeon240 <xnaxna1@naver.com>
2024-12-16 18:03:09 +0900

    [Config] Config 정적 파일 경로 추가

--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -82,9 +82,14 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("/img/");
+
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("/css/");
+
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("/js/");
     }
 
 }
