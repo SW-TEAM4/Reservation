8b983b23c4ca0d63d84633d0b0f0cb8090ad501b
gilbert <dlrlfgh517@gmail.com>
2025-01-02 22:33:05 +0900

    [Fix] LodReview lod_idx

--- src/main/java/kr/co/team4/controller/LodReviewController.java
+++ src/main/java/kr/co/team4/controller/LodReviewController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
@@ -18,16 +19,14 @@ public class LodReviewController {
 
     // 숙소 리뷰 통계 및 리뷰 목록 조회
     @GetMapping("/lodreview.do")
-    public String index(Model model) {
-
-        LodReviewDTO lodReviewDTO = new LodReviewDTO();
-        lodReviewDTO.setLod_idx(90); // 테스트용
+    public String index(@RequestParam(value = "lod_idx", required = false) int lodIdx,
+                        Model model) {
         // 통계 데이터 가져오기
-        Map<String, Object> statistics = lodReviewService.getReviewStatistics(lodReviewDTO.getLod_idx());
+        Map<String, Object> statistics = lodReviewService.getReviewStatistics(lodIdx);
         model.addAttribute("statistics", statistics);
 
         // 리뷰 목록 가져오기
-        List<LodReviewDTO> reviewList = lodReviewService.getReviewList(lodReviewDTO.getLod_idx());
+        List<LodReviewDTO> reviewList = lodReviewService.getReviewList(lodIdx);
         model.addAttribute("reviewList", reviewList);
 
         // 한 화면에서 표시
