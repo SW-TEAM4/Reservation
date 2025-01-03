b71c3b2f7df8e7426ad46f787764c5002d70c069
hyunseok99 <modesty66@naver.com>
2025-01-03 20:39:51 +0900

    [Fix] lodgmentDetail 쿼리 수정에 따른 Mapper, Service 수정

--- src/main/java/kr/co/team4/model/service/LodgmentServiceImpl.java
+++ src/main/java/kr/co/team4/model/service/LodgmentServiceImpl.java
@@ -17,11 +17,9 @@ public class LodgmentServiceImpl implements LodgmentService {
     private LodgmentMapper lodgmentMapper;
 
     @Override
-    public Map<String, Object> lodgmentDetail(LodgmentDTO lodgmentDTO) {
-        Map<String, Object> resultMap = new HashMap<>();
-        LodgmentDTO lodgmentDetail = lodgmentMapper.lodgmentDetail(lodgmentDTO).get(0); // 한 개의 데이터 반환
-        resultMap.put("lodgment", lodgmentDetail);  // 숙소 정보 담기
-        return resultMap;
+    public LodgmentDTO lodgmentDetail(LodgmentDTO lodgmentDTO) {
+        // 한 개의 데이터 반환
+        return lodgmentMapper.lodgmentDetail(lodgmentDTO);
     }
     @Override
     public List<RoomDTO> getRoomsByLodgment(LodgmentDTO lodgmentDTO) {
