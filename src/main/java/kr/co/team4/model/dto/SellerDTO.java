e5c127a8b55cb8872c4f1503a5bcf15ef1c51e48
hyunseok99 <modesty66@naver.com>
2025-01-03 17:50:17 +0900

    [Fix] 중복 어노테이션 제거

--- src/main/java/kr/co/team4/model/dto/SellerDTO.java
+++ src/main/java/kr/co/team4/model/dto/SellerDTO.java
@@ -6,14 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
 import java.math.BigInteger;
 
-@Data // @Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor 포함
+@Data
 @NoArgsConstructor // 기본 생성자 자동 생성
 @AllArgsConstructor // 모든 필드를 포함한 생성자 자동 생성
-import java.sql.Timestamp;
-
-@Data
 public class SellerDTO {
     private BigInteger SELLER_IDX;
     private String SELLER_NAME;
@@ -26,7 +24,5 @@ public class SellerDTO {
     private Timestamp CREATED;
     private Timestamp UPDATED;
     private String STATUS;
-
-
 
 }
