4a61472ddc0e99fe1c10da5f7f6c932f00548ae8
JM99 <hjm8565@gmail.com>
2025-01-07 14:26:40 +0900

    [Fix] login session 수정 [UI] userlogin, userregister CSS 수정

--- src/main/java/kr/co/team4/model/dto/SellerDTO.java
+++ src/main/java/kr/co/team4/model/dto/SellerDTO.java
@@ -5,15 +5,13 @@ package kr.co.team4.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.sql.Timestamp;
 import java.math.BigInteger;
+
 
 @Data // @Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor 포함
 @NoArgsConstructor // 기본 생성자 자동 생성
 @AllArgsConstructor // 모든 필드를 포함한 생성자 자동 생성
-import java.sql.Timestamp;
-
-@Data
 public class SellerDTO {
     private BigInteger SELLER_IDX;
     private String SELLER_NAME;
