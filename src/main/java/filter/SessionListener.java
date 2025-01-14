6698715a4f723714999db73ebd1f2c0bc6f94e65
hyunseok99 <modesty66@naver.com>
2025-01-14 16:50:59 +0900

    [Refactor] 사장님 페이지 인터셉터 널 체크 수정

--- src/main/java/filter/SessionListener.java
+++ src/main/java/filter/SessionListener.java
@@ -12,10 +12,14 @@ public class SessionListener implements HttpSessionListener {
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         HttpSession session = httpSessionEvent.getSession();
         // 세션 유지 시간 1시간
-        session.setMaxInactiveInterval(60 * 60);
+//        session.setMaxInactiveInterval(60 * 60);
+        System.out.println("새로운 세션 생성: 세션 ID: " + session.getId());
+        session.setMaxInactiveInterval(30);
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        HttpSession session = httpSessionEvent.getSession();
+        System.out.println("세션이 종료되었습니다. 세션 ID: " + session.getId());
     }
 }
